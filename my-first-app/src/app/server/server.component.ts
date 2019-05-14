81100a29bc085339d5c66439828571c7c1ba9830
Maik Switalski <Maik.Switalski@gmx.de>
2019-05-14 15:30:22 +0200

    Use property binding for styling (ngStyle)

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,11 +5,19 @@ import { Component } from '@angular/core';
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent{
-    serverId:number = 10;
-    serverStatus:string = 'offline';
+export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 
-    getServerStatus(){
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
