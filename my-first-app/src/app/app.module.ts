bc00db467ddc83a08c2a426af99deef3c3829fa4
Maik Switalski <Maik.Switalski@gmx.de>
2019-05-14 13:23:04 +0200

    Group multiple server.component into servers.component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
